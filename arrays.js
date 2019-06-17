876b8962f793c5bccef492dc5e0bf705be7b73b8
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-17 13:04:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray("foo", 1) {
+function addElementToBeginningOfArray('foo', 1) {
   chocolateBars.push();
   return chocolateBars;
 }
 
-function destructivelyAddElementToBeginningOfArray() {
+function destructivelyAddElementToBeginningOfArray('foo', 1) {
    chocolateBars[5] = " ";
   return chocolateBars;
 }
