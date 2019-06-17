fe77f4642e6559ef17d5c0f67f64f34d0a5a7aec
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-17 13:07:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray() {
-  chocolateBars.push('foo', 1);
+  chocolateBars.push('foo');
   return chocolateBars;
 }
 
