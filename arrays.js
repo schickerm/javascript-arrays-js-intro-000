10780015c9ec4583a5432dc8a8533de17466576c
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-17 13:02:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray() {
+  chocolateBars.push();
+  return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+   chocolateBars[5] = " ";
+  return chocolateBars;
+}
